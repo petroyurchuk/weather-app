de8fd5f6a171d762dcdb8195082f15675fa3755d
PetroYurchuk <petroyurchuk66@gmail.com>
2023-09-28 20:05:28 +0300

    project has been cleaned for the start of development

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-2xl font-bold text-slate-800 animate-bounce">
+        HERE WILL BE WEATHER
+      </div>
     </div>
   );
 }
